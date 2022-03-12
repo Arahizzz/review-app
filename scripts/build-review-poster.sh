59f7383966b0ab87e97ecf7e65e1a8525e7d1bb6
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2022-03-12 22:48:35 +0200

    Fix build binary script

--- scripts/build-review-poster.sh
+++ scripts/build-review-poster.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$SCRIPT_DIR/..
 
 cd $ROOT_DIR/review-poster && npm i
-npm run tsc-review-poster
+cd $ROOT_DIR && npm run tsc-review-poster
 pkg $ROOT_DIR/review-poster -o $ROOT_DIR/src-tauri/bin/review-poster    
 cd $ROOT_DIR && node $SCRIPT_DIR/move-binary.mjs
